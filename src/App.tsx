70478fdb40ea0d5bcfe7f4a7d43f2242a24a2eaf
Ninel <ninelgruzdeva@gmail.com>
2022-06-07 03:37:12 +0500

    refactor: change client to run apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import './App.css';
-import TodayWeather from "./components/TodayWeather";
-import WeekWeather from "./components/WeekWeather";
+import React from 'react'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import './App.css'
+import TodayWeather from './components/TodayWeather'
+import WeekWeather from './components/WeekWeather'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
+      <>
         <TodayWeather/>
         <WeekWeather/>
-    </>
+      </>
+    </ApolloProvider>
   );
 }
 
