eebaa6720c077e38b4ad8286633ee49bf16fc1ea
Ninel <ninelgruzdeva@gmail.com>
2022-06-08 00:49:29 +0500

    fix: add metric units to requests

--- server/src/datasources/weather-api.js
+++ server/src/datasources/weather-api.js
@@ -1,17 +1,21 @@
-const { RESTDataSource } = require('apollo-datasource-rest');
+const { RESTDataSource } = require("apollo-datasource-rest");
 
 class WeatherAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = 'https://api.openweathermap.org/data/2.5/';
+    this.baseURL = "https://api.openweathermap.org/data/2.5/";
   }
 
   getForecast(city) {
-    return this.get(`forecast?q=${city}&appid=${process.env.REACT_APP_API_KEY}`);
+    return this.get(
+      `forecast?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+    );
   }
 
   getCurrentWeather(city) {
-    return this.get(`weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`);
+    return this.get(
+      `weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+    );
   }
 }
 
