3aa2cfebd063d286108fe74360d38279ceb50fae
Ninel <ninelgruzdeva@gmail.com>
2022-06-07 23:43:12 +0500

    feat: add main queries to server

---
+++ src/queries/queries.js
@@ -0,0 +1,61 @@
+import { gql } from "@apollo/client";
+
+const GET_WEEK_FORECAST = gql`
+  query getForecast($city: String!) {
+    forecast(city: $city) {
+      cod
+      message
+      cnt
+      list {
+        dt
+        dt_txt
+        main {
+          temp
+        }
+        weather {
+          id
+          main
+          description
+          icon
+        }
+      }
+    }
+  }
+`;
+
+const GET_TODAY_WEATHER = gql`
+  query getCurrentWeather($city: String!) {
+    currentWeather(city: $city) {
+      id
+      dt
+      dt_txt
+      city {
+        name
+        country
+        sunrise
+        sunset
+      }
+      weather {
+        main
+        description
+        icon
+      }
+      main {
+        temp
+        feels_like
+        temp_min
+        temp_max
+        pressure
+        humidity
+      }
+      wind {
+        speed
+      }
+      clouds {
+        all
+      }
+    }
+  }
+`;
+
+export { GET_WEEK_FORECAST, GET_TODAY_WEATHER };
