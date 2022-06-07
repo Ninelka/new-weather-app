cfaa7d7ddb846338ced2f05f5e1867b07c48a43a
Ninel <ninelgruzdeva@gmail.com>
2022-06-08 00:33:59 +0500

    refactor: move apollo provider to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import React from 'react'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-import './App.css'
-import TodayWeather from './components/TodayWeather'
-import WeekWeather from './components/WeekWeather'
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
-})
+import React from "react";
+import "./App.css";
+import TodayWeather from "./components/TodayWeather";
+import WeekWeather from "./components/WeekWeather";
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <>
-        <TodayWeather/>
-        <WeekWeather/>
-      </>
-    </ApolloProvider>
+    <>
+      <TodayWeather />
+      <WeekWeather />
+    </>
   );
 }
 
