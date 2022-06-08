a8f0c06cd7fc3973398f706ec795fd35e07a7ff2
Ninel <ninelgruzdeva@gmail.com>
2022-06-08 23:54:19 +0500

    feat: add query result wrapper

---
+++ src/components/QueryResult.tsx
@@ -0,0 +1,28 @@
+import React, { ReactChildren } from "react";
+import { ApolloError } from "@apollo/client";
+
+interface QueryResultProps {
+  loading: boolean;
+  error?: ApolloError;
+  data: object;
+  children?: ReactChildren;
+}
+
+const QueryResult = ({
+  loading,
+  error,
+  data,
+  children,
+}: QueryResultProps): any => {
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>ERROR: {error}</p>;
+
+  if (!data) {
+    return <p>Nothing to show...</p>;
+  }
+  if (data) {
+    return children;
+  }
+};
+
+export default QueryResult;
