1562f807c6b8e7e2926b2441539c1bf4a124f7d7
Ninel <ninelgruzdeva@gmail.com>
2022-06-10 02:58:22 +0500

    fix: week forecast interface

--- src/interfaces/IWeekForecast.ts
+++ src/interfaces/IWeekForecast.ts
@@ -1,17 +1,8 @@
+import { IWeekForecastList } from "./IWeekForecastList";
+
 export interface IWeekForecast {
   cod: number;
   message: string;
   cnt: number;
-  list: {
-    dt: number;
-    dt_txt: string;
-    main: {
-      temp: number;
-    };
-    weather: {
-      main: string;
-      description: string;
-      icon: string;
-    };
-  };
+  list: IWeekForecastList[];
 }
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export type { IQueryVariable } from "./IQueryVariable";
 export type { ICurrentWeather } from "./ICurrentWeather";
 export type { IWeekForecast } from "./IWeekForecast";
+export type { IWeekForecastList } from "./IWeekForecastList";
