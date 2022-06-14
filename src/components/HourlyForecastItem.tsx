d5134d312ec77db2220d7d526af3d74d26948f6f
Ninel <ninelgruzdeva@gmail.com>
2022-06-15 01:12:07 +0500

    feat: add icon to forecast item

--- src/components/HourlyForecastItem.tsx
+++ src/components/HourlyForecastItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HourlyForecastItemProps } from "../interfaces";
+import { Icon } from "./Icon";
 
 const HourlyForecastItem: React.FC<HourlyForecastItemProps> = (props) => {
   const { date, icon, temperature } = props;
@@ -7,7 +8,7 @@ const HourlyForecastItem: React.FC<HourlyForecastItemProps> = (props) => {
   return (
     <div className="flex flex-col">
       <div>{date}</div>
-      <div>{icon}</div>
+      <Icon width={32} height={32} path={`assets/images/${icon}.svg`} />
       <div>{temperature.toFixed()}&deg;</div>
     </div>
   );
