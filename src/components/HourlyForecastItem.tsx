7801a86a3656d42ab200d5b3060cfa56574523f4
Ninel <ninelgruzdeva@gmail.com>
2022-06-16 02:54:01 +0500

    fix: hourly forecast background

--- src/components/HourlyForecastItem.tsx
+++ src/components/HourlyForecastItem.tsx
@@ -9,7 +9,7 @@ const HourlyForecastItem: React.FC<HourlyForecastItemProps> = (props) => {
   const formatDate = dayjs(date).format("h A");
 
   return (
-    <div className="flex flex-col items-center justify-between min-w-[60px] gap-3 py-4 px-2 bg-solid-1 rounded-[30px]">
+    <div className="bg-['rgba(72, 49, 157, .2)'] shadow-1 flex min-w-[60px] flex-col items-center justify-between gap-3 rounded-[30px] py-4 px-2">
       <span className="bold-subheadline text-dark-primary">{formatDate}</span>
       <Icon width={32} height={32} path={`assets/images/${icon}.svg`} />
       <div className="regular-title-3 text-dark-primary">
