3a614a5ad21b16c202920116148bb808de24f4cb
Ninel <ninelgruzdeva@gmail.com>
2022-06-16 02:56:13 +0500

    feat: add layout and Input to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import TodayWeather from "./components/TodayWeather";
 import WeekWeather from "./components/WeekWeather";
+import Layout from "./components/Layout";
+import Input from "./components/Input";
 
 function App() {
   const [city, setCity] = React.useState("");
@@ -22,15 +24,24 @@ function App() {
 
   return (
     <>
-      <div>
-        <label>Enter your city:</label>
-        <input type="text" onChange={(e) => setCity(e.target.value)} />
-        <button type="submit" onClick={getWeather}>
-          Get weather
-        </button>
-      </div>
-      {showCurrentWeather && <TodayWeather city={city} />}
-      {showForecast && <WeekWeather city={city} />}
+      <Layout>
+        <div className="flex items-end justify-between gap-6">
+          <Input
+            label="Enter your city:"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={getWeather}
+            className="bold-subheadline shadow-1 flex max-h-8 items-center justify-center whitespace-nowrap rounded-[30px] p-4 text-dark-primary"
+          >
+            Get weather
+          </button>
+        </div>
+        {showCurrentWeather && <TodayWeather city={city} />}
+        {showForecast && <WeekWeather city={city} />}
+      </Layout>
     </>
   );
 }
