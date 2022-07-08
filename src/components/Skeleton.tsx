f47c0de60ca1cc31122ee3a0577b83eb7e9bb4ef
Ninel <ninelgruzdeva@gmail.com>
2022-07-09 02:18:44 +0500

    refactor: add skeleton custom styles

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,8 +1,18 @@
+import classNames from "classnames";
 import React from "react";
 
-const Skeleton = () => {
+interface SkeletonProps {
+  className?: string;
+}
+
+const Skeleton: React.FC<SkeletonProps> = ({ className }) => {
   return (
-    <div className="relative h-full w-full overflow-hidden rounded-xl">
+    <div
+      className={classNames(
+        "relative h-full w-full overflow-hidden rounded-xl",
+        className
+      )}
+    >
       <div className="absolute top-0 left-0 h-full w-full">
         <div className="skeleton transform-center block h-full transition-transform"></div>
       </div>
